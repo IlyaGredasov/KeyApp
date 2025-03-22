78c6ce37bb43d846ac7b55564557f1fe4208e40e
IlyaGredasov <iljagredasov@yandex.ru>
2025-03-22 21:21:38 +0300

    feat: add first api version of http requests

--- src/WebApi.py
+++ src/WebApi.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
+from functools import wraps
 from random import random
 from typing import List
 
-from faker import Faker
+import requests
+import inspect
+from functools import wraps
 
 
 @dataclass
@@ -13,54 +16,63 @@ class PasswordQuery:
     isAdminProtected: bool = 0
 
 
-faker = Faker()
-n = 15
-adminPassword = "123"
+def preventiveRequest(url, method="get"):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            sig = inspect.signature(func)
+            bound = sig.bind(*args, **kwargs)
+            bound.apply_defaults()
+            funcArgs = dict(bound.arguments)
 
-db = [PasswordQuery(faker.domain_name(), faker.user_name(), faker.password())
-      for i in range(n)]
+            r = eval(f"requests.{method}")(url=f'http://10.55.0.1:8000/{url}',
+                                           json=funcArgs)
+            return func(*args, **kwargs, json=r.json())
+
+        return wrapper
+
+    return decorator
 
 
-def refreshMasterKey():
+@preventiveRequest(url="change_key", method="put")
+def refreshMasterKey(json=None) -> bool:
+    return json['status'] == "data_key_changed"
+
+
+@preventiveRequest(url="get_all", method="get")
+def getAll(json=None) -> List[PasswordQuery]:
+    return [PasswordQuery(username=query['login'], password=query['password'], domain=query['domain']) for query in
+            json['data_list']]
+
+
+@preventiveRequest(url="get_correct", method="get")
+def getByDomain(domain: str, json=None) -> PasswordQuery:
+    return PasswordQuery(domain=domain, username=json['login'], password=json['password'])
+
+
+@preventiveRequest(url="add_data", method="post")
+def addQuery(domain: str, login: str, password: str, isAdminProtected: bool = 0, json=None) -> bool:
+    return json['status'] == "data_added"
+
+
+@preventiveRequest(url="delete_data", method="delete")
+def deleteQuery(domain: str, json=None) -> bool:
+    return json['status'] == "data_deleted"
+
+
+@preventiveRequest(url="change_data", method="post")
+def changeQuery(domain: str, username: str, password: str, isAdminProtected: bool = 0, json=None) -> bool:
+    return json['status'] == "data_changed"
+
+
+def fuzzySearch(domain: str, json=None) -> List[PasswordQuery]:
+    return [query for query in getAll() if random() < 0.6]
+
+
+def checkAdminPassword(password: str, json=None) -> bool:
     return True
 
 
-def getAll() -> List[PasswordQuery]:
-    return db
-
-
-def getByDomain(domain: str) -> PasswordQuery:
-    return [query for query in db if query.domain == domain][0]
-
-
-def addQuery(domain: str, username: str, password: str, isAdminProtected: bool = 0) -> bool:
-    db.append(PasswordQuery(domain, username, password, isAdminProtected))
-    return True
-
-
-def deleteQuery(domain: str) -> bool:
-    for query in db:
-        if query.domain == domain:
-            db.remove(query)
-            return True
-    return False
-
-
-def changeQuery(domain: str, username: str, password: str, isAdminProtected: bool) -> bool:
-    for i in range(len(db)):
-        if db[i].domain == domain:
-            db[i] = PasswordQuery(domain, username, password, isAdminProtected)
-            return True
-    return False
-
-
-def fuzzySearch(domain: str):
-    return [query for query in db if random() < 0.6]
-
-
-def checkAdminPassword(password: str) -> bool:
-    return password == adminPassword
-
-
-def checkFlashDriver() -> bool:
-    return True
+@preventiveRequest(url="check_token", method="get")
+def checkFlashDriver(json=None) -> bool:
+    return json['status'] == "Token is found"
