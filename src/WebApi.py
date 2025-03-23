a8958592af336769ba3d9930fab74054d444d702
IlyaGredasov <iljagredasov@yandex.ru>
2025-03-23 19:59:21 +0300

    feat: second version of api

--- src/WebApi.py
+++ src/WebApi.py
@@ -16,7 +16,7 @@ class PasswordQuery:
     isAdminProtected: bool = 0
 
 
-def preventiveRequest(url, method="get"):
+def preventiveRequest(url, method):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -24,55 +24,82 @@ def preventiveRequest(url, method="get"):
             bound = sig.bind(*args, **kwargs)
             bound.apply_defaults()
             funcArgs = dict(bound.arguments)
-
-            r = eval(f"requests.{method}")(url=f'http://10.55.0.1:8000/{url}',
-                                           json=funcArgs)
-            return func(*args, **kwargs, json=r.json())
+            del funcArgs['json']
+            if 'isAdminProtected' in funcArgs.keys():
+                del funcArgs['isAdminProtected']
+            print(funcArgs)
+            print(wrapper.__name__)
+            try:
+                r = eval(f"requests.{method}")(url=f'http://10.55.0.1:8000/{url}',
+                                               json=funcArgs)
+                print(r.json())
+                return func(*args, **kwargs, json=r.json())
+            except requests.exceptions.RequestException as e:
+                print(f"An error occurred: {e}")
+                return False
 
         return wrapper
 
     return decorator
 
 
+@preventiveRequest(url="check_token", method="get")
+def checkToken(json=None) -> bool:
+    return json['status'] == "Token is found"
+
+
+def preventiveCheckToken(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if checkToken():
+            return func(*args, **kwargs)
+
+    return wrapper
+
+
+@preventiveCheckToken
 @preventiveRequest(url="change_key", method="put")
 def refreshMasterKey(json=None) -> bool:
     return json['status'] == "data_key_changed"
 
 
+@preventiveCheckToken
 @preventiveRequest(url="get_all", method="get")
 def getAll(json=None) -> List[PasswordQuery]:
     return [PasswordQuery(username=query['login'], password=query['password'], domain=query['domain']) for query in
             json['data_list']]
 
 
-@preventiveRequest(url="get_correct", method="get")
-def getByDomain(domain: str, json=None) -> PasswordQuery:
+@preventiveCheckToken
+@preventiveRequest(url="get_correct", method="post")
+def getByDomain(domain: str, login: str = "", password: str = "", json=None) -> PasswordQuery:
     return PasswordQuery(domain=domain, username=json['login'], password=json['password'])
 
 
+@preventiveCheckToken
 @preventiveRequest(url="add_data", method="post")
 def addQuery(domain: str, login: str, password: str, isAdminProtected: bool = 0, json=None) -> bool:
     return json['status'] == "data_added"
 
 
-@preventiveRequest(url="delete_data", method="delete")
-def deleteQuery(domain: str, json=None) -> bool:
+@preventiveCheckToken
+@preventiveRequest(url="delete_data", method="post")
+def deleteQuery(domain: str, login: str = "", password: str = "", json=None) -> bool:
     return json['status'] == "data_deleted"
 
 
+@preventiveCheckToken
 @preventiveRequest(url="change_data", method="post")
-def changeQuery(domain: str, username: str, password: str, isAdminProtected: bool = 0, json=None) -> bool:
+def changeQuery(domain: str, login: str, password: str, isAdminProtected: bool = 0, json=None) -> bool:
     return json['status'] == "data_changed"
 
 
-def fuzzySearch(domain: str, json=None) -> List[PasswordQuery]:
-    return [query for query in getAll() if random() < 0.6]
+@preventiveCheckToken
+@preventiveRequest(url="get_fuzzy", method="post")
+def fuzzySearch(domain: str, login: str = "", password: str = "", json=None) -> List[PasswordQuery]:
+    return [PasswordQuery(username=query['login'], password=query['password'], domain=query['domain']) for query in
+            json['data_list']]
 
 
 def checkAdminPassword(password: str, json=None) -> bool:
     return True
-
-
-@preventiveRequest(url="check_token", method="get")
-def checkFlashDriver(json=None) -> bool:
-    return json['status'] == "Token is found"
