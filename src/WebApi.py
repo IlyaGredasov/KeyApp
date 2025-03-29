46d1a4336601d66b2fdccce59343e9b6d0590ebb
IlyaGredasov <iljagredasov@yandex.ru>
2025-03-29 18:37:41 +0300

    fix: fix check token issue

--- src/WebApi.py
+++ src/WebApi.py
@@ -1,11 +1,10 @@
+import inspect
+import time
 from dataclasses import dataclass
 from functools import wraps
-from random import random
 from typing import List
 
 import requests
-import inspect
-from functools import wraps
 
 
 @dataclass
@@ -27,12 +26,13 @@ def preventiveRequest(url, method):
             del funcArgs['json']
             if 'isAdminProtected' in funcArgs.keys():
                 del funcArgs['isAdminProtected']
-            print(funcArgs)
-            print(wrapper.__name__)
             try:
+                print(func.__name__, funcArgs)
+                start = time.perf_counter()
                 r = eval(f"requests.{method}")(url=f'http://10.55.0.1:8000/{url}',
-                                               json=funcArgs)
-                print(r.json())
+                                               json=funcArgs, timeout=(10 if func.__name__ == "fuzzySearch" else 0.5, 10))
+                end = time.perf_counter()
+                print(f"Request {method} took {end - start:.6f} seconds")
                 return func(*args, **kwargs, json=r.json())
             except requests.exceptions.RequestException as e:
                 print(f"An error occurred: {e}")
@@ -48,53 +48,51 @@ def checkToken(json=None) -> bool:
     return json['status'] == "Token is found"
 
 
-def preventiveCheckToken(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        if checkToken():
-            return func(*args, **kwargs)
+def preventiveCheckToken(onFailure=None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if checkToken():
+                return func(*args, **kwargs)
+            elif onFailure:
+                onFailure()
 
-    return wrapper
+        return wrapper
+
+    return decorator
 
 
-@preventiveCheckToken
 @preventiveRequest(url="change_key", method="put")
 def refreshMasterKey(json=None) -> bool:
     return json['status'] == "data_key_changed"
 
 
-@preventiveCheckToken
 @preventiveRequest(url="get_all", method="get")
 def getAll(json=None) -> List[PasswordQuery]:
     return [PasswordQuery(username=query['login'], password=query['password'], domain=query['domain']) for query in
             json['data_list']]
 
 
-@preventiveCheckToken
 @preventiveRequest(url="get_correct", method="post")
 def getByDomain(domain: str, login: str = "", password: str = "", json=None) -> PasswordQuery:
     return PasswordQuery(domain=domain, username=json['login'], password=json['password'])
 
 
-@preventiveCheckToken
 @preventiveRequest(url="add_data", method="post")
 def addQuery(domain: str, login: str, password: str, isAdminProtected: bool = 0, json=None) -> bool:
     return json['status'] == "data_added"
 
 
-@preventiveCheckToken
 @preventiveRequest(url="delete_data", method="post")
 def deleteQuery(domain: str, login: str = "", password: str = "", json=None) -> bool:
     return json['status'] == "data_deleted"
 
 
-@preventiveCheckToken
 @preventiveRequest(url="change_data", method="post")
 def changeQuery(domain: str, login: str, password: str, isAdminProtected: bool = 0, json=None) -> bool:
     return json['status'] == "data_changed"
 
 
-@preventiveCheckToken
 @preventiveRequest(url="get_fuzzy", method="post")
 def fuzzySearch(domain: str, login: str = "", password: str = "", json=None) -> List[PasswordQuery]:
     return [PasswordQuery(username=query['login'], password=query['password'], domain=query['domain']) for query in
