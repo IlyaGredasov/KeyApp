a49a07789cff97f229cbffdbd366bcb193536ea1
IlyaGredasov <114508749+IlyaGredasov@users.noreply.github.com>
2025-04-27 21:31:39 +0300

    feat: add logging and app icon, fix bugs

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install libxcb-xinerama0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-render-util0 libxkbcommon-x11-0
-sudo apt install python3.12 python3.12-venv python3.12-dev
+sudo apt install python3.11 python3.11-venv python3.11-dev
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
@@ -9,12 +9,14 @@ pip install -r requirements.txt
 pyinstaller --onefile --name=KeyApp \
   --add-data="./src/Alert.py:." \
   --add-data="./src/DeleteDialog.py:." \
+  --add-data="./src/Logger.py" \
   --add-data="./src/PasswordQuery.py:." \
   --add-data="./src/WebApi.py:." \
   --add-data="./design/alert.png:." \
+  --add-data="./design/icon.svg:." \
   --collect-submodules PyQt5 \
   --collect-submodules requests \
-  --collect-binaries python3.12 \
+  --collect-binaries python3.11 \
   ./src/MainWindow.py
 
 deactivate
---
+++ src/Logger.py
@@ -0,0 +1,16 @@
+import sys
+import logging
+
+class LoggerWriter:
+    def __init__(self, logger, level):
+        self.logger = logger
+        self.level = level
+        self._buffer = ''
+
+    def write(self, message):
+        message = message.rstrip()
+        if message:
+            self.logger.log(self.level, message)
+
+    def flush(self):
+        pass
