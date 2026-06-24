742068b8dc364d30c22fcf646ab7112c44396ef8
IlyaGredasov <iljagredasov@yandex.ru>
2026-06-24 14:34:25 +0300

    build: migrate to uv

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,11 +2,9 @@
 
 sudo apt install libxcb-xinerama0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-render-util0 libxkbcommon-x11-0
 sudo apt install python3.11 python3.11-venv python3.11-dev
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
+uv sync --python 3.11 --locked
 
-pyinstaller --onefile --name=KeyApp \
+uv run pyinstaller --onefile --name=KeyApp \
   --add-data="./src/Alert.py:." \
   --add-data="./src/DeleteDialog.py:." \
   --add-data="./src/Logger.py" \
@@ -18,5 +16,3 @@ pyinstaller --onefile --name=KeyApp \
   --collect-submodules requests \
   --collect-binaries python3.11 \
   ./src/MainWindow.py
-
-deactivate
